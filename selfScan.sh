9b8971b6f86d449ca886d2e71c7b8c67ea7d6272
acomputerguy <copyandpaste0@gmail.com>
2016-09-25 22:08:02 -0700

    Create selfScan.sh

---
+++ selfScan.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#timestamp the file name
+fileName=("SelfScanResults%M-%d-%Y_%H:%M.xml")
+
+#automatically pull up IP address from etho0
+ipAddr=`ifconfig eth0 | grep "inet addr" | cut -d ':' -f 2 | cut -d ' ' -f 1`
+
+#scan and print results to an xml file
+scan_Results=`nmap -sV $ipAddr -oX $fileName`
+
+#parse through ndiff results and print out necessary information
+scan_Diff=`ndiff sample.xml $fileName | awk '/'"$ipAddr"'/{y=1;next}y'`
+
+#print out results to file
+echo "Report:" "$scan_Diff" > "report.txt"
