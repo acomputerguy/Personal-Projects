e11ece3f2e37567e16a33caef7b95863ee566785
acomputerguy <copyandpaste0@gmail.com>
2017-01-09 10:01:25 -0500

    Update perpetualScan.sh

--- Red-Blue-Team/perpetualScan.sh
+++ Red-Blue-Team/perpetualScan.sh
@@ -3,25 +3,27 @@
 #Script will constantly scan the local box and alert the user for port activity
 
 #Pre-conditions: Make sure the XML format is acceptable for ndiff (try a test case and make it original_Results.xml)
-
 #!/bin/bash
 
-#automatically pull up the IP address in use
+#Automatically pull up the IP address in use
 ipAddr=`ifconfig eth0 | grep "inet addr" | cut -d ':' -f 2 | cut -d ' ' -f 1`
 
 #counter for scan # in report
 i=0
 
 #very first file name
-oldFileName="original_Results.xml"
+oldFileName="OldResults.xml"
+newFileName="NewResults.xml"
+
+mkFile=`touch $oldFileName $newFileName report.log`
+
+printThis=`nmap -sV $ipAddr -oX $oldFileName`
 
 #endlessly scan the local machine
 while :
 do
         #timestamp file name and store for future reference
-	startfn=("SelfScanResults")
 	timeStamp=`date +%m-%d-%Y_%H:%M:%S`
-	newFileName=("$startfn$timeStamp.xml")
 
         #scan and print newer results to xml file
 	printThis=`nmap -sV $ipAddr -oX $newFileName`
@@ -29,12 +31,9 @@ do
         #parse through ndiff results and print out the essentials
 	scan_Diff=`ndiff $oldFileName $newFileName | awk '/'"$ipAddr"'/{y=1;next}y'`
 
-        #remove old file, no longer needed anymore
-	rm $oldFileName
-
         #the new file becomes the old file once we scan in the next iteration
-	oldFileName=$newFileName
-            
+	moveFile=`cat $newFileName > $oldFileName`
+
         #if there is no difference in between previous and current scan results,
         #go to next iteration and skip remainder of code below
 	if [ -z "$scan_Diff" ]
@@ -46,8 +45,4 @@ do
         #append results to file and increment counter by 1 with a timestamp
 	echo -e "$timeStamp Report #$i" "$scan_Diff\n" >> "report.log"
 	i=$(( i+1 ))
-
-        #delete all old results in trash bin
-	rm -rf ~/.local/share/Trash *
-
 done
