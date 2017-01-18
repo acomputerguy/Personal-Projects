7b5eccc896a9e42acc42baea7c6492116779d790
acomputerguy <copyandpaste0@gmail.com>
2017-01-18 06:56:17 +0000

    Create linuxScraper.sh

---
+++ Web-Scraper/linuxScraper.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+#check if there is at least 1 argument later
+wget $1
+
+#when downloaded, file name is diff
+#http://imgur.com/gallery/mMKha
+
+file=$( echo $1 | cut -f  5 -d '/' )
+
+media=$( cat $file | grep "<img src=\"//i.imgur.com/" | awk '{print $2;}' | cut -f 2 -d "=" | cut -c 4-26 )
+
+for i in "${media[@]}"
+do
+	wget $i
+done
