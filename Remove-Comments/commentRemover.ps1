819a66ef20c6940e3840f249fba56ab4b67571df
Felipe Webb <copyandpaste0@gmail.com>
2017-02-08 23:03:36 -0500

    Update commentRemover.ps1

--- Remove-Comments/commentRemover.ps1
+++ Remove-Comments/commentRemover.ps1
@@ -2,7 +2,7 @@ param (
     [string]$progFile
 )
 
-#acquire file extension
+#acquire file extension from command line arg
 $indexDot = $progFile.IndexOf('.')
 $fileNameLen = $progFile.Length
 $fileExtLen = $fileNameLen - $indexDot
@@ -21,10 +21,21 @@ foreach($line in Get-Content $absPath)
         if($line -match "(`"$fileExt| $fileExt | $fileExt`")")
         {
             Write-Host $line
+            $indexCommentLine = $line[$line.IndexOf('commentLine=') + 13]
         }
     }
 }
+Write-Host $indexCommentLine
+
+#loop through file mentioned in arg
+foreach($line in Get-Content $progFile)
+{
+    if($line -notmatch $indexCommnetLine)
+    {
+        
+    }
+}
+
+#type in: .\commentRemover.ps1 file_Name.ext
 #C:\Program Files (x86)\Notepad++\langs.model.xml
 #format goes: commentLine="//" commentStart="/*" commentEnd="*/">
-
-#grab commentLine
