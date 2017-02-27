d2bebdebb9b01af61ffa98d9643817d926b3ef5c
Felipe Webb <copyandpaste0@gmail.com>
2017-02-27 08:42:49 -0500

    Update webPermissions.sh

--- Web-Server-Code/webPermissions.sh
+++ Web-Server-Code/webPermissions.sh
@@ -4,7 +4,6 @@
 #Description: Search and set all files and directories in
 #current and subdirectories permissions accordingly for web server
 
-#home directory (/home/abc1234/) should be 711
 #before wq, :set ff=unix
 for i in $( find -iname '*' )
 do
